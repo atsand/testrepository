ab6a072649abfdb402d30d5a448dfdeeb0f505c4
Aaron Sandusky <atsand23@gmail.com>
2018-01-03 20:43:35 -0500

    first commit

---
+++ IntroToMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplaySalesTax(100);
+        }
+
+        private static void DisplaySalesTax(double salesAmount)
+        {
+            const double RATE = 0.07;
+            double tax = RATE * salesAmount;
+            Console.WriteLine("The tax on {0} is {1}.", salesAmount.ToString("C"), tax.ToString("C"));
+        }
+
+    }
+}
